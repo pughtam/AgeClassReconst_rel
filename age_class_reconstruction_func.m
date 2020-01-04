fb7d783cc10f50e9ab04b03c5a54cc1a90949f34
Thomas Pugh <t.a.m.pugh@bham.ac.uk>
2020-01-04 21:49:18 +0000

    Committing changes to a file that was missed in the first commit to this branch.

--- age_class_reconstruction_func.m
+++ age_class_reconstruction_func.m
@@ -1,31 +1,41 @@
 function fage_out_decade=age_class_reconstruction_func(use_dist_scen,LUC_age_weights,inc_dist,inc_luh2,dist_age_weights,output_years,...
     nages,secdf_init,primf_init,luh2_forlu_loss_sec_1deg,luh2_forlu_loss_prim_1deg,luh2_forlu_gain_1deg,distint,nyear,nspinup,year1,...
-    dist_scen_start,dist_scen_end,nyout,nages_dec)
+    dist_scen_start,dist_scen_end,nyout,nages_dec,firstscenyear,lastscenyear)
 %Make the calculations of how forest age changes over time.
 %To be called from age_class_reconstruction.m
-%Track primary and secondary forest from LUH2 separately and only merge together in the final output array.
+%Track primary and secondary forest from LUHv2h separately and only merge together in the final output array.
 %
 %T. Pugh
 %12.09.19
 
+firstscenind=firstscenyear-year1;
+lastscenind=lastscenyear-year1;
+
 %If using a disturbance rate scenario in time, initialise the annual multiplicative array here
 if use_dist_scen
     dist_scen=NaN(nyear,1);
-    for nn=1:nyear
+    for nn=1:firstscenind-1
+        dist_scen(nn)=dist_scen_start;
+    end
+    for nn=firstscenind:lastscenind
         df=dist_scen_end-dist_scen_start;
-        dist_scen(nn)=dist_scen_start+(nn*(df/nyear));
+        nnn=nn-firstscenind;
+        dist_scen(nn)=dist_scen_start+(nnn*(df/(lastscenind-firstscenind)));
+    end
+    for nn=lastscenind+1:nyear
+        dist_scen(nn)=dist_scen_end;
     end
     clear nn
 end
 
-%Set weight arrays for likelihood of LUC by age class
+%Set weight arrays for likelihood of LUC or wood harvest by age class
 if LUC_age_weights==0
     %Do nothing, no age weighting applied
 elseif LUC_age_weights==1
-    %Increased likelihood of LUC for older classes
+    %Increased likelihood of LUC for older classes (arbitrary increase to test sensitivity)
     weights_LUC=1:(4/(nages-1)):5;
 elseif LUC_age_weights==2
-    %Increased likelihood of LUC for younger classes
+    %Increased likelihood of LUC for younger classes (arbitrary increase to test sensitivity)
     weights_LUC=5:-(4/(nages-1)):1;
 else
     error('LUC_age_class not set to 0, 1 or 2')
@@ -38,10 +48,10 @@ end
 if dist_age_weights==0
     %Do nothing, no age weighting applied
 elseif dist_age_weights==1
-    %Increased likelihood of disturbance for older classes
+    %Increased likelihood of disturbance for older classes (arbitrary increase to test sensitivity)
     weights_dist=1:(4/(nages-1)):5;
 elseif dist_age_weights==2
-    %Increased likelihood of disturbance for younger classes
+    %Increased likelihood of disturbance for younger classes (arbitrary increase to test sensitivity)
     weights_dist=5:-(4/(nages-1)):1;
 else
     error('dist_age_class not set to 0, 1 or 2')
@@ -53,16 +63,17 @@ end
 %Settings for desired output years
 fage_out_decade=NaN(360,180,nages_dec,nyout);
 
+%Initialise diagnostic arrays to track whether there are any unallocated transitions for the LUH2 data
 prim_unallocated=zeros(360,180);
 sec_unallocated=zeros(360,180);
 
-%Loop over latitudes to make the array size acceptable
+%Loop over latitudes to make the array size manageable in memory
 llint=20; %index range for latitude bands
 
 for lls=1:llint:180
     fprintf('%d\n',lls)
     
-    %Initialise forest fractions as the maximum age
+    %Initialise forest fractions at the maximum age (i.e. all old-growth prior to the beginning of the nage-long spin-up period)
     fage_sec=zeros(360,llint,nages);
     fage_sec(:,:,nages)=secdf_init(:,lls:lls+llint-1);
     fage_prim=zeros(360,llint,nages);
@@ -70,15 +81,14 @@ for lls=1:llint:180
     
     fage=zeros(360,llint,nages,nyear+nspinup);
     
-    %Now start the loop for forest age calculation
+    %Now start the year loop for forest age calculation
     for yy=1:nyear+nspinup
-        %fprintf('%d\n',yy)
         
-        %First increment the ages of forest
+        %First increment the ages of forest by one year
         %Secondary forest
-        fage_sec(:,:,nages)=fage_sec(:,:,nages)+fage_sec(:,:,nages-1);
-        fage_sec(:,:,2:nages-1)=fage_sec(:,:,1:nages-2);
-        fage_sec(:,:,1)=zeros(360,llint);
+        fage_sec(:,:,nages)=fage_sec(:,:,nages)+fage_sec(:,:,nages-1); %Adding to old-growth
+        fage_sec(:,:,2:nages-1)=fage_sec(:,:,1:nages-2); %All intermediate ages increment by one age class
+        fage_sec(:,:,1)=zeros(360,llint); %First age class must become zero
         %Primary forest
         fage_prim(:,:,nages)=fage_prim(:,:,nages)+fage_prim(:,:,nages-1);
         fage_prim(:,:,2:nages-1)=fage_prim(:,:,1:nages-2);
@@ -86,21 +96,22 @@ for lls=1:llint:180
         
         if inc_luh2
             %Subtract forest loss
-            %Remove losses from random age class until all losses are allocated
+            %Remove losses from a randomly-chosen age class until all losses are allocated
             
             for ii=1:360
-                for jj=1:llint
-                    jjj=lls-1+jj;
+                for jj=1:llint %Index within current latitude band
+                    jjj=lls-1+jj; %Index across all latitude bands
                     
                     %Secondary forest
                     if luh2_forlu_loss_sec_1deg(ii,jjj,yy)>0
-                        to_lose=luh2_forlu_loss_sec_1deg(ii,jjj,yy); %Running total of lost forest fraction still to be allocated
+                        to_lose=luh2_forlu_loss_sec_1deg(ii,jjj,yy); %Initialise running total of lost forest fraction still to be allocated
                         while to_lose>0.00000001
                             hf=find(fage_sec(ii,jj,:)>0); %Only select the ages with forest cover in order to optimise the calculation
                             if ~isempty(hf)
+                                sec_carryover=0;
+                                
                                 if LUC_age_weights==0
                                     %Randomly choose age class to remove forest from, weighted by fraction of forest in age class to avoid abnormally low probabilities for old growth
-                                    %rr=randi(length(hf));
                                     rr=randsample(length(hf),1,true,fage_sec(ii,jj,hf));
                                 elseif LUC_age_weights==1 || LUC_age_weights==2
                                     %Randomly choose age class with modified likelihood by age class
@@ -109,16 +120,13 @@ for lls=1:llint:180
                                 
                                 if fage_sec(ii,jj,hf(rr)) > to_lose
                                     fage_sec(ii,jj,hf(rr))=fage_sec(ii,jj,hf(rr))-to_lose;
-                                    sec_carryover=0;
                                     break
                                 else
                                     to_lose=to_lose-fage_sec(ii,jj,hf(rr));
                                     fage_sec(ii,jj,hf(rr))=0;
-                                    sec_carryover=0;
                                 end
                             else
                                 %If there is no secondary forest to lose then ignore this loss
-                                %fprintf('WARNING: secondary forest loss greater than current fraction %f\n',to_lose)
                                 sec_unallocated(ii,jjj)=sec_unallocated(ii,jjj)+to_lose;
                                 sec_carryover=to_lose; %Attempt to carryover unallocated losses to primary forest
                                 break
@@ -129,13 +137,12 @@ for lls=1:llint:180
                     
                     %Primary forest
                     if luh2_forlu_loss_prim_1deg(ii,jjj,yy)>0
-                        to_lose=luh2_forlu_loss_prim_1deg(ii,jjj,yy)+sec_carryover; %Running total of lost forest fraction still to be allocated
+                        to_lose=luh2_forlu_loss_prim_1deg(ii,jjj,yy)+sec_carryover; %Initialise running total of lost forest fraction still to be allocated
                         while to_lose>0.00000001
                             hf=find(fage_prim(ii,jj,:)>0);
                             if ~isempty(hf)
                                 if LUC_age_weights==0
                                     %Randomly choose age class to remove forest from, weighted by fraction of forest in age class to avoid abnormally low probabilities for old growth
-                                    %rr=randi(length(hf));
                                     rr=randsample(length(hf),1,true,fage_prim(ii,jj,hf));
                                 elseif LUC_age_weights==1 || LUC_age_weights==2
                                     %Randomly choose age class, with modified likelihood by age class
@@ -151,7 +158,6 @@ for lls=1:llint:180
                                 end
                             else
                                 %If there is no primary forest to lose then ignore this loss
-                                %fprintf('WARNING: primary forest loss greater than current fraction %f\n',to_lose)
                                 prim_unallocated(ii,jj)=prim_unallocated(ii,jj)+to_lose;
                                 break
                             end
@@ -184,7 +190,7 @@ for lls=1:llint:180
             end
             
             if dist_age_weights==0
-                %If doing a uniform disturbance then can do this simply across the arrays
+                %If applying a uniform disturbance then can do this as a simple multiplication across the arrays
                 
                 %Secondary forest
                 frac_dist_sec=fage_sec(:,:,2:nages).*repmat(distrate,[1 1 nages-1]);
